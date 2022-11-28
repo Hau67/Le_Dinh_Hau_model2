f8de43f0b584ba1b50e474e9647643542ef30bad
Hau67 <dinhhau671012@gmail.com>
2022-11-28 13:42:14 +0700

    first commit

---
+++ src/ss16/baitap/IllegalTriangleException.java
@@ -0,0 +1,14 @@
+package ss16.baitap;
+
+public class IllegalTriangleException extends Exception {
+    double a;
+    double b;
+    double c;
+
+    public IllegalTriangleException() {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+}
---
+++ src/ss16/baitap/Triangle.java
@@ -0,0 +1,33 @@
+package ss16.baitap;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" hình tam giác");
+        System.out.println("nhập cạnh a");
+        double a = Integer.parseInt(sc.nextLine());
+        System.out.println(" nhập cạnh b");
+        double b = Integer.parseInt(sc.nextLine());
+        System.out.println(" nhập cạnh c");
+        double c = Integer.parseInt(sc.nextLine());
+        Triangle triangle = new Triangle();
+
+        try{
+            triangle.check(a,b,c);
+        } catch (IllegalTriangleException e) {
+            System.out.println(e.getMessage());
+        }
+        }
+
+    private void check(double a, double b, double c) throws IllegalTriangleException {
+        if (a > 0 && b > 0 && c > 0 && (a + b > c || a + c > b || b + c > a)) {
+            System.out.println("Đây là 1 tam giác");
+        } else {
+            System.out.println("sai điều kiên");;
+        }
+    }
+}
+
+
